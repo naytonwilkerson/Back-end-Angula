9f890eb0c231d47357f71013c546536d8a67be1b
NaytonWilkerson <naytonwilkerson78@gmail.com>
2019-04-07 12:37:56 -0300

    Entidade Cliente e Empresa implementados

---
+++ src/main/java/com/example/demo/ProjetoAngularApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAngularApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/domain/Cliente.java
@@ -0,0 +1,107 @@
+package com.example.demo.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Cliente implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer codigo;
+	private String nome;
+	private String linguagem;
+	private String cidade;
+	private String pais;
+	private String telefone;
+	
+	
+	
+	
+	public Cliente() {
+		super();
+	}
+
+	public Cliente(Integer codigo, String nome, String linguagem, String cidade, String pais,
+			String telefone) {
+		super();
+		this.codigo = codigo;
+		this.nome = nome;
+		this.linguagem = linguagem;
+		this.cidade = cidade;
+		
+		this.pais = pais;
+		this.telefone = telefone;
+		
+	}
+	
+	//getset
+
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public String getLinguagem() {
+		return linguagem;
+	}
+
+	public void setLinguagem(String linguagem) {
+		this.linguagem = linguagem;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+
+	public String getPais() {
+		return pais;
+	}
+
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	
+	
+	
+	
+	
+	
+	
+
+}
